577188469fb648830bd235884990a2160f9c5c5b
Xapollon430 <Xapollon430@gmail.com>
2023-02-20 17:00:02 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ export default function DrawerAppBar() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/triage" element={<Triage />} />
-        <Route path="/cpt" element={<Triage />} />
+        {/* <Route path="/cpt" element={<Triage />} /> */}
       </Routes>
     </>
   );
